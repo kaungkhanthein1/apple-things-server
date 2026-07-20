ed2c8d4b19b63b791cece8a94b0a576c91af601e
kkh <kaungkhantheinkkh2003@gmail.com>
2026-07-20 23:34:42 +0700

    auth module fix

--- medusa-config.ts
+++ medusa-config.ts
@@ -138,14 +138,6 @@ module.exports = defineConfig({
   modules: {
     auth: {
       resolve: "@medusajs/auth",
-      dependencies: ["customer"],
-      options: {
-        providers: [
-          {
-            name: "emailpass",
-          },
-        ],
-      },
     },
     file: {
       resolve: "@medusajs/medusa/file",
